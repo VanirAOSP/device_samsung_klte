ff1844aa6db7e6d2ada76b1ca65f0377ced43cf3
nuclearmistake <nuclearmistake@gmail.com>
2014-07-13 10:08:07 -0400

    Reconcile wifi w/ elelinux
    Change-Id: I81590cf55ca0d8ca23d0f8bf6fd12512e5d11354

--- device.mk
+++ device.mk
@@ -205,8 +205,8 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-   $(LOCAL_PATH)/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+   $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+   $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
 # call common msm8974
 $(call inherit-product, device/samsung/msm8974-common/msm8974.mk)
