0f1cffa65a4b5c79170e1aa1e30e27e2a387b203
nuclearmistake <nuclearmistake@gmail.com>
2014-07-27 11:13:38 -0400

    try twrp
    Change-Id: I6f77a5d0a66bcf777093dc27b5e59855ab4a0785

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,15 +43,47 @@ BOARD_CUSTOM_BOOTIMG_MK := device/samsung/klte/mkbootimg.mk
 # Graphics
 BOARD_EGL_CFG := device/samsung/klte/egl.cfg
 
-# Recovery
+ifeq ($(RECOVERY_VARIANT),)
+RECOVERY_VARIANT := twrp
+endif
+
+# Recovery (common)
+TARGET_RECOVERY_FSTAB := device/samsung/klte/rootdir/etc/fstab.qcom
+
+ifeq ($(RECOVERY_VARIANT),twrp)
+# Recovery (twrp)
+DEVICE_RESOLUTION := 1080x1920
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_SAMSUNG := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p26"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic wait,check,encryptable=footer"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+#TW_BRIGHTNESS_PATH := /sys/devices/mdp.0/qcom,mdss_fb_primary.190/leds/lcd-backlight/brightness
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun0/file
+TWRP_EVENT_LOGGING := true
+else
+# Recovery (cwm - unused)
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/klte/recovery/recovery_keys.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+endif
+
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := device/samsung/klte/rootdir/etc/fstab.qcom
 
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 11534336
